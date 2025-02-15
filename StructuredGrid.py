abfae06e6b036a28610a47b01dae600cc56a15f2
LethalOtter <alecdestrada@gmail.com>
2025-02-14 20:01:57 -0600

    First working version of structured grid generator for the pipe with sine curve bumps

--- StructuredGrid.py
+++ StructuredGrid.py
@@ -1,3 +1,5 @@
+# %%
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -21,8 +23,9 @@ class StructuredGrid:
         self.XiGrid = None
         self.xGrid = None
         self.yGrid = None
-        self.delta_eta = 1 / numJNodes
-        self.delta_xi = 1 / numINodes
+        self.Coeffs = None
+        self.delta_eta = 1 / (numJNodes - 1)
+        self.delta_xi = 1 / (numINodes - 1)
 
     def ComputationalGrid(self):
 
@@ -32,69 +35,133 @@ class StructuredGrid:
         def Eta(self, j):
             return j / (self.numJNodes - 1)
 
-        self.EtaGrid = [
-            [Eta(self, j) for i in range(self.numINodes)] for j in range(self.numJNodes)
-        ]
+        self.EtaGrid = []
+        for j in range(self.numJNodes):
+            for i in range(self.numINodes):
+                self.EtaGrid.append(Eta(self, j))
 
-        self.XiGrid = [
-            [Xi(self, i) for i in range(self.numINodes)] for j in range(self.numJNodes)
-        ]
+        self.XiGrid = []
+        for j in range(self.numJNodes):
+            for i in range(self.numINodes):
+                self.XiGrid.append(Xi(self, i))
 
         return self.EtaGrid, self.XiGrid
 
-    def AlgebraicGrid(self, xLeft, xRight, yLower, yUpper):
+    def MakeAlgebraicGrid(self, xLeft, xRight, yLower, yUpper):
         def yConversion(eta):
             return yLower + eta * (yUpper - yLower)
 
         def xConversion(xi):
             return xLeft + xi * (xRight - xLeft)
 
-        self.yGrid = [[yConversion(eta) for eta in row] for row in self.EtaGrid]
+        self.yGrid = [yConversion(eta) for eta in self.EtaGrid]
 
-        self.xGrid = [[xConversion(xi) for xi in row] for row in self.XiGrid]
+        self.xGrid = [xConversion(xi) for xi in self.XiGrid]
+
+        # y Boundary conditions
+        for l in range(self.numINodes):
+            if self.xGrid[l] <= 2 or self.xGrid[l] >= 3:
+                continue
+
+            self.yGrid[l] = 0.17 * np.sin((self.xGrid[l] - 2) * np.pi)
+            self.yGrid[self.numINodes * (self.numJNodes - 1) + l] = 1 - 0.17 * np.sin(
+                (self.xGrid[l] - 2) * np.pi
+            )
 
         return self.yGrid, self.xGrid
 
-    def LaplaceGrid(self):
+    def CalculateCoefficients(self, l):
+        dx_deta = (self.xGrid[l + self.numINodes] - self.xGrid[l - self.numINodes]) / (
+            2 * self.delta_eta
+        )
 
-        yGridNew = np.zeros_like(self.yGrid)
-        xGridNew = np.zeros_like(self.xGrid)
+        dx_dxi = (self.xGrid[l + 1] - self.xGrid[l - 1]) / (2 * self.delta_xi)
 
-        def dy_deta(j, i, yGrid):
-            return (yGrid[j + 1, i] - yGrid[j - 1, i]) / (2 * self.delta_eta)
+        dy_deta = (self.yGrid[l + self.numINodes] - self.yGrid[l - self.numINodes]) / (
+            2 * self.delta_eta
+        )
 
-        def dy_dxi(j, i, yGrid):
-            return (yGrid[j, i + 1] - yGrid[j, i - 1]) / (2 * self.delta_xi)
+        dy_dxi = (self.yGrid[l + 1] - self.yGrid[l - 1]) / (2 * self.delta_xi)
 
-        def dx_deta(j, i, xGrid):
-            return (xGrid[j + 1, i] - xGrid[j - 1, i]) / (2 * self.delta_eta)
+        alpha = dx_deta**2 + dy_deta**2
+        beta = (dx_dxi * dx_deta) + (dy_dxi * dy_deta)
+        gamma = dx_dxi**2 + dy_dxi**2
 
-        def dx_dxi(j, i, xGrid):
-            return (xGrid[j, i + 1] - xGrid[j, i - 1]) / (2 * self.delta_xi)
+        A = (alpha * self.delta_xi**2) / (
+            2 * alpha * self.delta_eta**2 + 2 * gamma * self.delta_xi**2
+        )
+        B = (beta * self.delta_xi * self.delta_eta) / (
+            8 * alpha * self.delta_eta**2 + 8 * gamma * self.delta_xi**2
+        )
+        C = (gamma * self.delta_xi**2) / (
+            2 * alpha * self.delta_eta**2 + 2 * gamma * self.delta_xi**2
+        )
 
-        def alpha(j, i):
-            return dx_deta(j, i) ** 2 + dy_deta(j, i) ** 2
+        # print(f"{alpha=}")
+        # print(f"{beta=}")
+        # print(f"{gamma=}")
 
-        def beta(j, i):
-            return (dx_dxi(j, i) * dx_deta(j, i)) + (dy_dxi(j, i) * dy_deta(j, i))
+        return -B, C, B, A, A, B, C, -B
 
-        def gammaa(j, i):
-            return dx_dxi(j, i) ** 2 + dy_dxi(j, i) ** 2
+    def GaussSeidel(self, tol):
+        tol
+        error = True
+        while error:
+            errors = []
 
-        # define 'a' coefficients
-        # iterate through array
-        # crash out
+            newYGrid = self.yGrid.copy()
+
+            # Filtering out rows and columns containing boundary conditions
+            for l, y in enumerate(self.yGrid):
+                if l // self.numINodes < 1:  # Skipping bottom row
+                    continue
+                if l // self.numINodes == self.numJNodes - 1:  # Skipping top row
+                    continue
+                if l % self.numINodes < 1:  # Skipping left column
+                    continue
+                if l % self.numINodes == self.numINodes - 1:  # Skipping right column
+                    continue
+
+                Coeffs = self.CalculateCoefficients(l)
+
+                newY = (
+                    Coeffs[0] * newYGrid[l - self.numINodes - 1]
+                    + Coeffs[1] * newYGrid[l - self.numINodes]
+                    + Coeffs[2] * newYGrid[l - self.numINodes + 1]
+                    + Coeffs[3] * newYGrid[l - 1]
+                    + Coeffs[4] * self.yGrid[l + 1]
+                    + Coeffs[5] * self.yGrid[l + self.numINodes - 1]
+                    + Coeffs[6] * self.yGrid[l + self.numINodes]
+                    + Coeffs[7] * self.yGrid[l + self.numINodes + 1]
+                )
+
+                errors.append(np.abs(self.yGrid[l] - newY))
+
+                newYGrid[l] = newY
+
+            self.yGrid = newYGrid
+            maxError = max(errors)
+            if maxError < tol:
+                error = False
+
+        return
 
 
 if __name__ == "__main__":
-    myGrid = StructuredGrid(5, 6)
+    tolerance = 1e-6
+    myGrid = StructuredGrid(30, 30)
     etaGrid, xiGrid = myGrid.ComputationalGrid()
-    yGrid, xGrid = myGrid.AlgebraicGrid(0, 5, 0, 6)
+    myGrid.MakeAlgebraicGrid(2, 3, 0, 1)
 
-    plt.figure(1)
-    plt.scatter(xGrid, yGrid)
+    # plt.figure(1)
+    # plt.scatter(myGrid.xGrid, myGrid.yGrid)
+    # plt.title("Iteration Zero")
+    # plt.axis("equal")
 
-    plt.figure(1)
-    plt.scatter(xiGrid, etaGrid)
+    myGrid.GaussSeidel(tolerance)
+
+    plt.figure(2)
+    plt.scatter(myGrid.xGrid, myGrid.yGrid)
+    # plt.axis("equal")
 
     plt.show()
